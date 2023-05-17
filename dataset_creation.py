602f3696644ceb863b8d42eb296e21262a1b196f
siddharth200119 <siddramnani@gmail.com>
2023-05-17 15:19:22 +0530

    added way to loop through database

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,4 +1,11 @@
+import normalization
+import detection
+import os
 
-
-
-
+for root, dirs, files in os.walk("Dataset\\"):
+    for name in files:
+        if name.endswith((".mp4", ".MP4")):
+            print(os.path.join(root, name))
+            path = (os.path.join(root, name)).split("\\")
+            n = len(path)
+            print(path[n-2])
